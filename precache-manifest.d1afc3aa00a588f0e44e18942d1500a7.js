ecb635e8875cb8a40edf9c8eda18400265280b5b
Yuwin98 <yuwinsapumana212@gmail.com>
2020-03-11 06:35:01 +0530

    Updates

---
+++ precache-manifest.d1afc3aa00a588f0e44e18942d1500a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1485b52ab60f6bcfa5cb008d1ea556",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa7677ea7ac221d12279",
+    "url": "/monsters-rolodex/static/css/main.760acfd3.chunk.css"
+  },
+  {
+    "revision": "f6cbc1f38bb15a18c333",
+    "url": "/monsters-rolodex/static/js/2.fa020b78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.fa020b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7677ea7ac221d12279",
+    "url": "/monsters-rolodex/static/js/main.72839573.chunk.js"
+  },
+  {
+    "revision": "c9c7304d86f32d1200e0",
+    "url": "/monsters-rolodex/static/js/runtime-main.cc127d2c.js"
+  }
+]);
